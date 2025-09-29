de4d25ecf601261b3fcceb52d3b82cf0eec7ecbd
Godson Pius <40927597+godson-pius@users.noreply.github.com>
2025-09-29 15:59:37 +0100

    worked on auth and header for admin dashboard

---
+++ src/app/admin/auth/page.tsx
@@ -0,0 +1,71 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+
+/* eslint-disable @next/next/no-img-element */
+export default function Page() {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+  return (
+    <main className="w-full h-screen flex">
+      {/*Image*/}
+      <div className="hidden md:block w-[604px] h-full bg-[url('/auth.svg')] bg-center bg-cover"></div>
+      <section className="flex justify-center items-center w-[53rem]">
+        <form className="flex flex-col w-[440px] items-center">
+          <img src={"/logo-black.svg"} alt="Logo" className="w-[90px]" />
+          <div className="title flex flex-col items-end mb-10 mt-5">
+            <h1 className="font-playfair w-[250px] text-center text-[22px] md:w-96 lg:w-full md:text-[32px] text-yard-primary font-bold md:leading-[56px] tracking-[-0.1px]">
+              Login
+            </h1>
+            <img
+              src={"/about-line.svg"}
+              alt="Line"
+              className="md:-mt-4 -mt-2 w-40 md:w-22"
+            />
+          </div>
+
+          <div className="input-group flex flex-col w-full">
+            <div className="flex flex-col gap-2">
+              <label htmlFor="email" className="text-[#1A1A1A] leading-6">
+                Enter your email address
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Enter your email address"
+                className="md:w-full h-[52px] rounded2px border-[1px] focus:outline-yard-dark-primary border-[#BFBFBF] p-3"
+              />
+            </div>
+          </div>
+
+          <div className="input-group flex flex-col w-full mt-5 relative">
+            <div className="flex flex-col gap-2">
+              <label htmlFor="password" className="text-[#1A1A1A] leading-6">
+                Enter your password
+              </label>
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                placeholder="Enter your password"
+                className="md:w-full h-[52px] rounded2px border-[1px] focus:outline-yard-dark-primary border-[#BFBFBF] p-3"
+              />
+              <img
+                onClick={() => setShowPassword((prev) => !prev)}
+                src={showPassword ? "/icons/eye-slash.svg" : "/icons/eye.svg"}
+                alt="Eye Logo"
+                className="w-6 absolute top-12 right-5 cursor-pointer"
+              />
+            </div>
+          </div>
+
+          <Link
+            href={"/admin/dashboard"}
+            className="w-full flex justify-center cta-btn bg-yard-primary text-yard-milk group relative overflow-hidden mt-5"
+          >
+            <span className="z-40">Login to dashboard</span>
+            <div className="absolute top-0 left-0 bg-yard-dark-primary w-full h-full transition-all duration-500 -translate-x-full group-hover:translate-x-0"></div>
+          </Link>
+        </form>
+      </section>
+    </main>
+  );
+}
---
+++ src/app/admin/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import Header from "@/components/dashboard/Header";
+
+export default function Page() {
+  return (
+    <main>
+      <Header />
+    </main>
+  );
+}
