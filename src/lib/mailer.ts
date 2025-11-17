5fa2c3f634c3dcb105732e49d664c1843ea54d07
Godson Pius <40927597+godson-pius@users.noreply.github.com>
2025-11-17 14:56:31 +0100

    doing final test

--- src/lib/mailer.ts
+++ src/lib/mailer.ts
@@ -6,88 +6,98 @@ import SMTPTransport from "nodemailer/lib/smtp-transport";
 // import { render } from "@react-email/render";
 // import BookingConfirmationEmail from "@/emails/BookingConfirmation";
 
-
 const transporter = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
-    port: Number(process.env.MAIL_PORT) || 587,
-    auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
-    },
+  host: process.env.MAIL_HOST,
+  port: Number(process.env.MAIL_PORT) || 587,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
 } as SMTPTransport.Options);
 
 export async function sendBookingEmail(to: string) {
-    // const html = render(<BookingConfirmationEmail name={ name } bookingId = { bookingId } />);
-    const html = `<p> Booking successful </B>`;
-    await transporter.sendMail({
-        from: `"The Yard" <${process.env.MAIL}>`,
-        to,
-        subject: "Booking submitted, complete payment to confirm",
-        html,
-    });
-};
+  // const html = render(<BookingConfirmationEmail name={ name } bookingId = { bookingId } />);
+  const html = `<p> Booking successful </B>`;
+  await transporter.sendMail({
+    from: `"The Yard" <${process.env.MAIL}>`,
+    to,
+    subject: "Booking submitted, complete payment to confirm",
+    html,
+  });
+}
 
 export async function sendBookingConfirmedEmail(to: string) {
-    // const html = render(<BookingConfirmationEmail name={ name } bookingId = { bookingId } />);
-    const html = `<p> Booking Confirmed </B>`;
-    await transporter.sendMail({
-        from: `"The Yard" <${process.env.MAIL}>`,
-        to,
-        subject: "We have received your payment and confirmed booking",
-        html,
-    });
-};
+  // const html = render(<BookingConfirmationEmail name={ name } bookingId = { bookingId } />);
+  const html = `<p> Booking Confirmed </B>`;
+  await transporter.sendMail({
+    from: `"The Yard" <${process.env.MAIL}>`,
+    to,
+    subject: "We have received your payment and confirmed booking",
+    html,
+  });
+}
 
-export async function sendMail(to: string, message: string, subject: string, customer: ICustomer) {
-    const html = `
+export async function sendMail(
+  to: string,
+  message: string,
+  subject: string,
+  customer: ICustomer,
+) {
+  const html = `
         <p> <strong> Hello ${customer.firstname} </strong> </p>
 
         <p> ${message} </p>
     `;
-    await transporter.sendMail({
-        from: `"The Yard" <${process.env.MAIL}>`,
-        to,
-        subject,
-        html,
-    });
+  await transporter.sendMail({
+    from: `"The Yard" <${process.env.MAIL}>`,
+    to,
+    subject,
+    html,
+  });
 }
 
 export async function inviteAdminEmail(admin: IAdmin, password: string) {
-    const html = `
+  const html = `
         <p> email: ${admin.email} </p>
         <p> password: ${password} </p>
         <p> role: ${admin.role} </p>
-    `
-    await transporter.sendMail({
-        from: `"The Yard" <${process.env.MAIL}>`,
-        to: admin.email,
-        subject: "The Yard Admin Invite",
-        html,
-    });
+    `;
+  await transporter.sendMail({
+    from: `"The Yard" <${process.env.MAIL}>`,
+    to: admin.email,
+    subject: "The Yard Admin Invite",
+    html,
+  });
 }
 
-export async function sendNotificationEmail(admin: IAdmin, data: INotification) {
-    const html = `
+export async function sendNotificationEmail(
+  admin: IAdmin,
+  data: INotification,
+) {
+  const html = `
         <p> email: ${admin.email} </p>
         <p> data: ${data} </p>
-    `
-    await transporter.sendMail({
-        from: `"The Yard" <${process.env.MAIL}>`,
-        to: admin.email,
-        subject: `${data.message}`,
-        html,
-    });
+    `;
+  await transporter.sendMail({
+    from: `"The Yard" <${process.env.MAIL}>`,
+    to: admin.email,
+    subject: `${data.message}`,
+    html,
+  });
 }
 
-export async function sendConfirmationEmail(email: IAdmin["email"], code: string) {
-    const html = `
+export async function sendConfirmationEmail(
+  email: IAdmin["email"],
+  code: string,
+) {
+  const html = `
         Admin Email Verification Code,
         < p > Your verification code is <b>${code} </b>. It expires in 10 minutes.</p >
-    `
-    await transporter.sendMail({
-        from: `"The Yard" <${process.env.MAIL}>`,
-        to: email,
-        subject: `Admin Email Verification Code`,
-        html,
-    });
+    `;
+  await transporter.sendMail({
+    from: `"The Yard" <${process.env.MAIL}>`,
+    to: email,
+    subject: `Admin Email Verification Code`,
+    html,
+  });
 }
